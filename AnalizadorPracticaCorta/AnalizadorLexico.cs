ab80252f378f80b7bdd809467ea9f0eec356402d
David15Barrera <68833254+David15Barrera@users.noreply.github.com>
2020-08-20 03:25:45 -0600

    Creación de los archivos

---
+++ AnalizadorPracticaCorta/AnalizadorLexico.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static AnalizadorPracticaCorta.MainWindow;
+
+namespace AnalizadorPracticaCorta
+{
+    class AnalizadorLexico
+    {
+        public AnalizadorLexico()
+        {
+
+        }
+        string palabra = "";
+
+        public String DatoInicial(String cadena)
+        {
+            String retorno = " ";
+            int numero = cadena.Length;
+            MessageBox.Show("Cantidad de letras " + numero);
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i].Equals(' '))
+                {
+                    Reconocer(palabra);
+                    palabra = "";
+                }
+                else
+                {
+                    palabra += cadena[i].ToString();
+                }
+            }
+            retorno = Reconocer(palabra);
+            palabra = "";
+            return retorno;
+        }
+
+        // Almacena las palabras
+        String palabraSuelta;
+        public String Reconocer(String palabra)
+        {
+            palabraSuelta += Resolver(palabra) + "\n";
+            return palabraSuelta;
+        }
+        /// <summary>
+        /// Segun lo identificado en Dato inicial y mostrado en el reconocer
+        /// nos vamos ha ver que tipo de dato es el que se almaceno
+        /// </summary>
+        /// <param name="palabra"></param>
+        /// <returns></returns>
+        public String Resolver(String palabra)
+        {
+            String letras = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+            String numeros = "1234567890";
+            String respuesta = "Identificado " + palabra + " No especificado ";
+
+            int contador_letra = 0;
+            int contador_numero = 0;
+            int contador_punto = 0;
+
+            // Identificado si es una palabra 
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                for (int j = 0; j < letras.Length; j++)
+                {
+                    if (palabra[i].Equals(letras[j]))
+                    {
+                        contador_letra++;
+                        Console.WriteLine(contador_letra);
+                    }
+                }
+            }
+            // Identificando si es un numero
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                for (int j = 0; j < numeros.Length; j++)
+                {
+                    if (palabra[i].Equals(numeros[j]))
+                    {
+                        contador_numero++;
+                    }
+                }
+            }
+            // Error de punto
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (palabra[i].Equals('.'))
+                {
+                    contador_punto++;
+                }
+             }
+            // ALmacenamiento de que tipo de identificador es
+            if (contador_letra == palabra.Length)
+            {
+                respuesta = "Identificador " +  palabra + "  es una Palabra ";        
+                       }
+            if (contador_numero == palabra.Length)
+            {
+                respuesta = "Identificador " + palabra + " es un Número Entero";
+                
+            }
+
+            if (contador_punto == 1 && contador_numero == (palabra.Length - 1))
+            {
+                respuesta = "Identificador " +  palabra + " es un Número Decimal";
+            }
+
+            if (contador_numero == (palabra.Length - 2) && contador_punto == 1 && palabra[0].Equals('Q'))
+            {
+                respuesta = "Identificador " + palabra + " es una Moneda";
+            }
+            return respuesta;
+        }
+    }
+}
---
+++ AnalizadorPracticaCorta/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnalizadorPracticaCorta
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+            private void Analizar_Click(object sender, RoutedEventArgs e)
+        {
+            //GenerarLatabla           
+            generarTabla();
+            //Llamar a la clase
+            AnalizadorLexico palabra = new AnalizadorLexico();
+           // Instruccion para mandar los datos a la tabla
+            lVresultados.Items.Add(new items { Lexema = palabra.DatoInicial(txbOracion.Text)});
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txbOracion.Text = " " ;
+            lVresultados.Items.Clear();
+        }
+
+        public class items
+        {
+            //atributos de la tabla
+            public string Lexema { get; set; }
+        }
+        public void generarTabla()
+        {
+            // Agrega las columnas
+            var gridView = new GridView();
+            lVresultados.View = gridView;
+            gridView.Columns.Add(new GridViewColumn
+            {
+                //Atributos de las columnas
+                Header = "Lexema",
+                DisplayMemberBinding = new Binding("Lexema"),
+                Width = 300
+
+            });
+       }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void lbInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Autor: David Enrique Lux Barrera Carnet 201931344 \n" +
+                " \n Ingrese el el cuadro de texto una oracion cualquiera \n" +
+                " Despues precione el boton de Analizar en su pantalla \n" +
+                " y en la tabla de resultados a parecera el analisis léxico \n" +
+                " de su oracion.\n" +
+                " Si quiere seguir analizando oraciones, presione limpiar para limpiar \n" +
+                " la tabla de resultados y así de esa manera trabajar mas \n" +
+                " ordenado.");
+        }
+    }
+}
